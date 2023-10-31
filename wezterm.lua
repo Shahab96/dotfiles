3ae789a0594e6438c3168bbb7e49e6a6b3c5f9d4
Shahab Dogar <shahab@dogar.dev>
2023-10-31 08:33:32 -0700

    Wezterm config

---
+++ wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.color_scheme = 'tokyonight_night'
+config.font = wezterm.font_with_fallback { 'ComicCodeLigatures', 'JetBrains Mono' }
+config.font_size = 16.0
+config.enable_tab_bar = false
+
+return config
