027a51e3450414fd862372582a34e3afcfa78a01
Shahab Dogar <shahab@dogar.dev>
2023-11-04 19:11:29 -0700

    Change wezterm font

--- wezterm.lua
+++ wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 local config = {}
 
 config.color_scheme = 'tokyonight_night'
-config.font = wezterm.font_with_fallback { 'ComicCodeLigatures', 'JetBrains Mono' }
+config.font = wezterm.font_with_fallback { 'ComicCode', 'JetBrains Mono' }
 config.font_size = 16.0
 config.enable_tab_bar = false
 
